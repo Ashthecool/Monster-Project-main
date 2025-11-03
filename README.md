6ba4b99150a06679c017018cf0fd4f26a40c4997
AshTheCoolest <kastvigasbjorn@gmail.com>
2025-11-03 14:24:34 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Monster-Project
+This website contains all the best monster energy, that brings out the monster in you. Yarg.
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Horror Search</title>
+    <link rel="stylesheet" href="styles.css">
+    <script src="script.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+    
+</head>
+<body>
+    <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+
+        $conn = new mysqli($servername, $username, $password);
+
+        // Check connection
+        if ($conn->connect_error) {
+        echo "<script>console.log('Connection failed: " . $conn->connect_error . "');</script>";
+        }
+        echo "<script>console.log('Connected successfully');</script>";
+    ?>
+
+</body>
+</html>
