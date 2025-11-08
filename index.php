7090200f6027470d0e996950d941a4deaf1bb6cc
HrafnFlokiP <hrafnfloki@gmail.com>
2025-11-08 10:12:50 +0100

    Fixes

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Horror Search</title>
     <link rel="stylesheet" href="styles.css">
-    <script src="script.js"></script>
+    <script src="scripts.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
     
 </head>
@@ -23,15 +23,15 @@
         }
         echo "<script>console.log('Connected successfully');</script>";
     ?>
-    <h1>sign up</h1>
+    <!-----------------login------------------>
     <button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
 
     <div id="id01" class="modal">
     
-    <form class="modal-content animate" action="/action_page.php" method="post">
+    <form class="modal-content" action="/action_page.php" method="post">
         <div class="imgcontainer">
         <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-        <img src="img_avatar2.png" alt="Avatar" class="avatar">
+        
         </div>
 
         <div class="container">
@@ -53,6 +53,40 @@
         </div>
     </form>
     </div>
+<!--------------------------------------------->
+<!--------------------sign up------------------->
+    <button onclick="document.getElementById('id02').style.display='block'" style="width:auto;">Sign Up</button>
+
+    <div id="id02" class="modal">
+    <span onclick="document.getElementById('id02').style.display='none'" class="close" title="Close Modal">&times;</span>
+    <form class="modal-content" action="/action_page.php">
+        <div class="container">
+        <h1>Sign Up</h1>
+        <p>Please fill in this form to create an account.</p>
+        <hr>
+        <label for="email"><b>Email</b></label>
+        <input type="text" placeholder="Enter Email" name="email" required>
+
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required>
+
+        <label for="psw-repeat"><b>Repeat Password</b></label>
+        <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+        
+        <label>
+            <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+        </label>
+
+        <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+
+        <div class="clearfix">
+            <button type="button" onclick="document.getElementById('id02').style.display='none'" class="cancelbtn2">Cancel</button>
+            <button type="submit" class="signupbtn">Sign Up</button>
+        </div>
+        </div>
+    </form>
+    </div>
+    <!-------------------------------------------->
         
 
 </body>
