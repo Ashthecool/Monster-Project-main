16ba191a2259f4d9ea5f09084af327413410c147
AshTheCoolest <kastvigasbjorn@gmail.com>
2025-11-17 13:52:38 +0100

    Add main.php structure and link CSS for styling

--- main_files/main.php
+++ main_files/main.php
@@ -13,66 +13,91 @@
     </header>
 
     <main>
-        <?php
-            // Database connection (update credentials)
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $dbname = "monster_energy";
+            <?php
+                // Database connection (update credentials)
+                $servername = "localhost";
+                $username = "root";
+                $password = "";
+                $dbname = "monster_energy";
 
-            mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-            $conn = new mysqli($servername, $username, $password, $dbname);
+                mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+                $conn = new mysqli($servername, $username, $password, $dbname);
 
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
 
-            $sql = "SELECT * FROM `monster_drinks`";
-            $result = $conn->query($sql);
+                $sql = "SELECT * FROM `monster_drinks`";
+                $result = $conn->query($sql);
 
+            ?>
+
+        <?php 
+        $list_drinks = array('1.webp', '3.png', '5.webp', '2.webp', '9.png', '31.webp', '22.webp', '23.png', '33.webp', '20.webp')
         ?>
-    </main>
-
-    <?php 
-    $list_drinks = array('1.webp', '3.png', '5.webp', '2.webp', '9.png', '31.webp', '22.webp', '23.png', '33.webp', '20.webp')
-    ?>
-    <div class="Slider" style="border: solid rgba(0, 0, 0, 0.468); 5px;">
-    <div class="slide-track">
-        <?php
-            foreach ($list_drinks as $d) {
-                echo <<<HTML
-                <div class='Material' style="border: solid black 5px;">
-                    <img 
-                        src='assets/drinks/$d' 
-                        alt='Drink $d' 
-                        class='drink'
-                    >
-                    <div class='middle'>
-                        <div class="text">Press for more info</div>
+        <div class="Slider" style="border: solid rgba(0, 0, 0, 0.468); 5px;">
+        <div class="slide-track">
+            <?php
+                foreach ($list_drinks as $d) {
+                    echo <<<HTML
+                    <div class='Material' style="border: solid black 5px;">
+                        <img 
+                            src='assets/drinks/$d' 
+                            alt='Drink $d' 
+                            class='drink'
+                        >
+                        <div class='middle'>
+                            <div class="text">Press for more info</div>
+                        </div>
                     </div>
-                </div>
-            HTML;
-            }
+                HTML;
+                }
 
-            // duplicate for smooth loop
-            foreach ($list_drinks as $d) {
-                echo <<<HTML
-                <div class='Material' style="border: solid black 5px;">
-                    <img 
-                        src='assets/drinks/$d' 
-                        alt='Drink $d' 
-                        class='drink'
-                    >
-                    <div class='middle'>
-                        <div class="text">Press for more info</div>
+                // duplicate for smooth loop
+                foreach ($list_drinks as $d) {
+                    echo <<<HTML
+                    <div class='Material' style="border: solid black 5px;">
+                        <img 
+                            src='assets/drinks/$d' 
+                            alt='Drink $d' 
+                            class='drink'
+                        >
+                        <div class='middle'>
+                            <div class="text">Press for more info</div>
+                        </div>
                     </div>
-                </div>
-            HTML;
-            }
-        ?>
+                HTML;
+                }
+            ?>
+        
+        
+        </div>
     </div>
-    </div>
+    
+    <br><br><br>
 
+    <h2 style="color: white; font-size: 40px; text-align: left;">What is this for?</h2>
+    <p style="color: white; font-size: 20px; text-align: left; margin-right: 800px;">
+        Creating a site where people can suggest new Monster Energy
+        flavors gives fans a place to unleash their chaotic creativity while giving us a steady stream
+        of insights into what people actually want. It builds hype, strengthens the community, and
+        doubles as low-cost research since the voting and comments reveal which ideas have real
+        traction. Plus, it opens doors for viral moments, limited-edition collabs, and a loyal user
+        base that feels involved instead of ignored.
+    </p>
+    <h2 style="color: white; font-size: 40px; text-align: right;">What can you do?</h2>
+    <p style="color: white; font-size: 20px; text-align: right; margin-left: 800px;">
+        In this place you can search for existing monsters <br>
+        You can make your own suggestions <br>
+        You can search existing monster suggestions <br>
+        You can comment on existing monsters
+    </p>
+    <div class="container">
+        <button class="Search">Search</button>
+        <button class="Request">Request Tastes</button>
+        <button class="Credits">Who made this?</button>
+    </div>
+</main>
 
 
 </body>
